d092a140ebaed47d1825af58d05cc1dcc7776b65
Remy Bouteloup <remy.bouteloup@etu.univ-tours.fr>
2018-05-25 11:56:08 +0200

    Add files via upload

--- ImageTagging_.java
+++ ImageTagging_.java
@@ -10,6 +10,7 @@ import java.awt.Color;
 import ij.process.ImageStatistics;
 import java.io.FilenameFilter;
 import java.util.ArrayList;
+import ij.plugin.ContrastEnhancer;
 
 public class ImageTagging_ implements PlugInFilter {
 
@@ -17,11 +18,10 @@ public class ImageTagging_ implements PlugInFilter {
 		ArrayList<Color> listColors = new ArrayList<Color>();
 		listColors.add(Color.black);
 		listColors.add(Color.blue);
-		listColors.add(Color.cyan);
-		listColors.add(Color.gray);
 		listColors.add(Color.green);
 		listColors.add(Color.orange);
 		listColors.add(Color.pink);
+		listColors.add(Color.red);
 		listColors.add(Color.white);
 		listColors.add(Color.yellow);
 
@@ -36,16 +36,19 @@ public class ImageTagging_ implements PlugInFilter {
 			    	PrintWriter writer = new PrintWriter("Images/"+files[i].getName()+".txt", "UTF-8");
 			    	writer.close();
 
-
 			    	// Création d’une image temporaire
 				    ImagePlus tempImg = new ImagePlus(files[i].getAbsolutePath());
 				    ImageProcessor ip = tempImg.getProcessor();
 
 			    	int width = ip.getWidth(), height = ip.getHeight();
 					int[] rgb = new int[3];
-					long rouge = 0, vert = 0, bleu = 0, size = width * height, meilleurDistance = 0, distance = 0;
-					long rougeMoyenne = 0, vertMoyenne = 0, bleuMoyenne = 0;
-					Color color = new Color(100,100,100);
+					double meilleurDistance = 0, distance = 0;
+					int[] counter = new int[10];
+					
+					for (int j = 0; j < 10; j++)
+						counter[j] = 0;
+					
+					int indexTemp = 0;
 
 					// Parcours chacun des pixels
 					for (int y = 0; y < height; y++)
@@ -54,46 +57,52 @@ public class ImageTagging_ implements PlugInFilter {
 						{
 							ip.getPixel(x,y,rgb); // Récupération d'un pixel à la position x et y
 
-							rouge += rgb[0];
-							vert += rgb[1];
-							bleu += rgb[2];
-						}
-					}
+							// Recherche la couleur la plus similaire à celle calculée
+							int index = 0;
+							for(Color c : listColors)
+							{
+								// Calcul de la distance entre la couleur calculée et une couleur de référence (voir l'arraylist)
+								Color color = new Color(rgb[0], rgb[1], rgb[2]);
 
-					// Calcul des moyennes
-					rougeMoyenne = rouge / size;
-					vertMoyenne = vert / size;
-					bleuMoyenne = bleu / size;
+								distance = distanceEuclidienne(color, c);
 
-
-					// Recherche la couleur la plus similaire à celle calculée
-					int index = 0;
-					for(Color c : listColors)
-					{
-						// Calcul de la distance entre la couleur calculée et une couleur de référence (voir l'arraylist)
-						distance = Math.abs(rougeMoyenne - c.getRed()) + Math.abs(vertMoyenne - c.getGreen()) + Math.abs(bleuMoyenne - c.getBlue());
-
-						//IJ.showMessage(Long.toString(rougeMoyenne));
-						//IJ.showMessage(Long.toString(vertMoyenne));
-						//IJ.showMessage(Long.toString(bleuMoyenne));
-
-						if (index == 0)
-							meilleurDistance = distance;
-						else
-							if (distance < meilleurDistance) {
-								meilleurDistance = distance;
-								color = new Color(c.getRed(), c.getGreen(), c.getBlue());
+								if (index == 0) {
+									meilleurDistance = distance;
+									indexTemp = index;
+								}
+								else
+									if (distance < meilleurDistance) {
+										meilleurDistance = distance;
+										indexTemp = index;
+									}
+									
+								index++;
 							}
-							
-						index++;
-					}
-					IJ.showMessage(color.toString());
+							counter[indexTemp] += 1;
+						}
+					}					
 		   		}
 
 		    } catch (IOException e) {
 			  	IJ.showMessage("Impossible de créer les fichiers textes, annulation du tagging");
 		    }
    		}
+	}
+
+	public double distance(Color c1, Color c2)
+	{
+		int red1 = c1.getRed();
+	    int red2 = c2.getRed();
+	    int rmean = (red1 + red2) >> 1;
+	    int r = red1 - red2;
+	    int g = c1.getGreen() - c2.getGreen();
+	    int b = c1.getBlue() - c2.getBlue();
+	    return Math.sqrt((((512+rmean)*r*r)>>8) + 4*g*g + (((767-rmean)*b*b)>>8));
+	}
+
+	public double distanceEuclidienne(Color c1, Color c2)
+	{
+		return Math.sqrt(Math.pow(c2.getRed() - c1.getRed(), 2) + Math.pow(c2.getGreen() - c1.getGreen(), 2) + Math.pow(c2.getBlue() - c1.getBlue(), 2));
 	}
 
 
